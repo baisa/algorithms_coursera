36d8a005f489d91a90ff998ef4a553ae1c646c67
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-02-04 16:47:12 +0100

    implementation in Ruby

--- quick_union.rb
+++ quick_union.rb
@@ -22,3 +22,30 @@
    # id[i] = j;
   #}   
 #}
+
+def setarray(num)
+  array = Array.new(num) { |i| i }
+end
+
+def root(array, x)
+  while x != array[x]
+    x = array[x]
+  end
+  x
+end
+
+def connected(array, x, y)
+  root(array, x) == root(array, y)
+end
+
+def union(array, x, y)
+  i = root(array, x)
+  j = root(array, y)
+  array[i] = j
+
+end
+
+a = setarray(10)
+r = root(a, 3)
+union(a, 1, 2)
+puts connected(a, 1, 2)
